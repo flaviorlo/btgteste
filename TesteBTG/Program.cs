efd9466aef08fa986812987caf001c5d7bf391b5
DESKTOP-2K20R39\Janaina <foliveira@frtisolucoes.com.br>
2023-03-03 23:11:35 -0300

    projto teste BTG

---
+++ TesteBTG/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Data;
+using TesteBTG;
+
+namespace TestBTG
+{
+    class program
+    {
+        static void Main()
+        {
+
+            string tipo = "Papel";
+            DataTable dtb = new DataTable();
+            dtb.Columns.Add("tipo");
+            dtb.Columns.Add("descricao");
+
+            dtb.Rows.Add("Tesoura", "Tesoura vence papel");
+            dtb.Rows.Add("Pedra", "Pedra vence tesoura");
+            dtb.Rows.Add("Papel", " papel vence pedra");
+
+            var query = from dados in dtb.AsEnumerable()
+                        where dados.Field<string>("tipo") == tipo
+                        select new
+                        {
+                            descricao=dados.Field<string>("descricao")
+                        };
+
+            Console.WriteLine("Item carregado : " + query.First().descricao);
+        } 
+
+    }
+}
---
+++ TesteBTG/jogada.cs
@@ -0,0 +1,25 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteBTG
+{
+    public class jogada
+    {
+        //pedra vence tesoura / tesour vence papel / papel vence pedra
+
+        public string realiza(string tipo)
+        {
+
+
+            return null; // result.Table.Columns[1].ToString();
+        }
+
+
+
+    }
+}
